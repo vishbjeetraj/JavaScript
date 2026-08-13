ac5fe02fe720a274e39a621621a02574f3f16d44
vishbjeetraj <vishbjeetraj67@gmail.com>
2026-08-13 11:11:01 +0530

    Topic Objects

---
+++ 02Basics/objects.js
@@ -0,0 +1,11 @@
+const jrUser = {
+          name : "Vishbjeet Raj",
+          gamil : "vishbjeetraj@gmail.com",
+          age : 19,
+          loccation: "AHEMEDABAD ",
+          userloggedIn : true,
+          lastLoggedIn : ["Monday","Wednesday"]
+}
+console.log(jrUser["gamil"]);
+
+
